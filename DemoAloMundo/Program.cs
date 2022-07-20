831c2fb0ef55e7f4074e35432f0578c10f902562
Vittoria Pr <84206474+vittpr@users.noreply.github.com>
2022-07-20 17:22:08 -0300

    Add files via upload

---
+++ DemoAloMundo/Program.cs
@@ -0,0 +1,29 @@
+﻿/* byte b;
+b = byte.MaxValue;
+Console.WriteLine("Valor máximo de byte:" + b);
+
+int i;
+i = int.MaxValue;
+Console.WriteLine("Valor máximo de int:" + i);
+
+long l;
+l = long.MaxValue;
+Console.WriteLine("Valor máximo de long:" + l);
+
+string strPrimeira = "Alo ";
+string strSegunda = "Mundo";
+
+string strTerceira = strPrimeira + strSegunda;
+Console.WriteLine(strTerceira);
+
+int cchTamanho = strTerceira.Length;
+string strQuarta = "Tamanho =" + cchTamanho.ToString();
+Console.WriteLine(strQuarta); */
+
+int i = 10;
+float f = 0;
+f = i;
+Console.WriteLine(f);
+f = 0.5f;
+i = (int)f;
+Console.WriteLine(i);
