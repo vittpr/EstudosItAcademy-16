405ee82b9644e7cdff9d567e3c451a1bb10cd779
Vittoria Pr <84206474+vittpr@users.noreply.github.com>
2022-07-26 17:41:50 -0300

    Add files via upload

---
+++ Laboratorio10/Pessoa.cs
@@ -0,0 +1,14 @@
+public class Pessoa
+{
+    public bool Casada {get; set;}
+
+    public string Nome {get; set;} = default!;
+
+    public DateTime DataNascimento {get; set;}
+
+    public override string ToString()
+    {
+        return $"[Nome = {Nome}, Casada = {Casada}, Data de nascimento = {DataNascimento.ToShortDateString()}]";
+        
+    }
+}
